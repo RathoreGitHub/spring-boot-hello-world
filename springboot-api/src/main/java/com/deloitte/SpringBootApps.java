72a55b01ecb5021a5ae9492df8b4c4ae7c836b0d
Rathore <abrathore@deloitte.com>
2018-03-07 16:31:36 +0530

    Hello World Spring boot application

---
+++ springboot-api/src/main/java/com/deloitte/SpringBootApps.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.deloitte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author abrathore
+ *
+ */
+@SpringBootApplication
+public class SpringBootApps {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringBootApps.class, args);
+		
+
+	}
+
+}
